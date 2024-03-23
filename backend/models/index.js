8183adbf96d83162836f64c63e2e57c5c5fd7ce7
Mohsin370 <mohsinijaz13@gmail.com>
2024-03-24 06:14:21 +1030

    docker setup, env variable in docker pending

--- backend/models/index.js
+++ backend/models/index.js
@@ -1,20 +1,17 @@
 'use strict';
-
+require("dotenv").config();
 const fs = require('fs');
 const path = require('path');
 const Sequelize = require('sequelize');
 const process = require('process');
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
+// const config = require(__dirname + '/../config/config.json')[env];
 const db = {};
-
-let sequelize;
-if (config.use_env_variable) {
-    sequelize = new Sequelize(process.env[config.use_env_variable], config);
-} else {
-    sequelize = new Sequelize(config.database, config.username, config.password, config);
-}
+let sequelize = new Sequelize(process.env.PG_DB, process.env.PG_USER, process.env.PG_PASSWORD, {
+    host: process.env.PG_HOST,
+    dialect: 'postgres'
+});
 
 fs
     .readdirSync(__dirname)
